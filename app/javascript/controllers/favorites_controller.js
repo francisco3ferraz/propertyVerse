250d71a2f5949fd5c7a745d467984cc40fc1b4f8
francisco <ffranciscofferraz@gmail.com>
2023-02-01 21:01:05 +0000

    commit24

--- app/javascript/controllers/favorites_controller.js
+++ app/javascript/controllers/favorites_controller.js
@@ -1,14 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
+import axios from 'axios';
 
 export default class extends Controller
 {
     favorite() {
+
+        if (this.element.dataset.userLoggedIn === 'false') {
+            return document.querySelector('[data-header-target="userAuthLink"]').click();
+        }
+
         if (this.element.dataset.favorited === 'true') {
-            this.element.dataset.favorited = 'false';
-            this.element.setAttribute('fill', 'rgba(0, 0, 0, 0.5)');
+            axios.delete(this.element.dataset.unfavoriteUrl, {     
+                headers: {
+                    'ACCEPT': 'application/json'
+                }
+            }).then((response) => {
+                this.element.dataset.favorited = 'false';
+                this.element.setAttribute('fill', 'rgba(0, 0, 0, 0.5)');
+            })
         } else {
-            this.element.setAttribute('fill', '#F47174');
-            this.element.dataset.favorited = 'true';
+            axios.post(this.element.dataset.favoriteUrl, {
+                user_id: this.element.dataset.userId,
+                property_id: this.element.dataset.propertyId
+            }, { 
+                headers: {
+                    'ACCEPT': 'application/json'
+                }
+            }).then((response) => {
+                this.element.setAttribute('fill', '#F47174');
+                this.element.dataset.favorited = 'true';
+            });
         }
     }
 }
