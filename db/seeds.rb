1d29244d09ce945fb998a11a5e2172fbb68dbc24
Francisco Ferraz <ffranciscofferraz@gmail.com>
2023-03-02 20:12:50 +0000

    commit-39

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,19 @@
 Faker::Config.locale = 'pt'
 
+user_pictures = []
+
+6.times do
+    user_pictures << URI.parse(Faker::LoremFlickr.image).open
+end
+
+me = User.create(email: "francisco@teste.com", password: "password", first_name: "Francisco", last_name: "Ferraz")
+me.picture.attach(io: user_pictures[0], filename: "#{me.full_name}.jpg")
+
+5.times do |i|
+    user = User.create(email: Faker::Internet.email, password: "password", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+    user.picture.attach(io: user_pictures[i + 1], filename: "#{user.full_name}.jpg")
+end
+
 10.times do |i|
     property = Property.create!(
         name: Faker::Lorem.unique.word,
@@ -15,7 +29,7 @@ Faker::Config.locale = 'pt'
     property.images.attach(io: File.open(Rails.root.join("db", "sample", "images", "property#{i + 1}.png")), filename: property.name)
     
     (1..5).to_a.sample.times do
-        Review.create(reviewable: property, rating: (1..5).to_a.sample, title: Faker::Lorem.word, body: Faker::Lorem.paragraph)
+        Review.create(reviewable: property, rating: (1..5).to_a.sample, title: Faker::Lorem.word, body: Faker::Lorem.paragraph, user: User.all.sample)
     end
 
 end
