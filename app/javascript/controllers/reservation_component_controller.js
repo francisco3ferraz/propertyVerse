1c6bd6e577d5f288a6bdb479945d636e52f7a4c8
Francisco Ferraz <ffranciscofferraz@gmail.com>
2023-03-04 11:04:41 +0000

    commit-43

--- app/javascript/controllers/reservation_component_controller.js
+++ app/javascript/controllers/reservation_component_controller.js
@@ -1,9 +1,10 @@
 import { Controller } from '@hotwired/stimulus';
 import { Datepicker } from 'vanillajs-datepicker';
+import { isEmpty } from 'lodash-es'
 
 export default class extends Controller
 {
-    static targets = ['checkin', 'checkout'];
+    static targets = ['checkin', 'checkout', 'numOfNights', 'nightlyTotal', 'serviceFee', 'total'];
     connect()
     {
         const checkinPicker = new Datepicker(this.checkinTarget, { 
@@ -20,6 +21,7 @@ export default class extends Controller
             checkoutPicker.setOptions({
                 minDate: date
             });
+            this.updateNightlyTotal();
         });
 
         this.checkoutTarget.addEventListener('changeDate', (e) => {
@@ -28,7 +30,47 @@ export default class extends Controller
             checkinPicker.setOptions({
                 maxDate: date
             });
+            this.updateNightlyTotal();
         });
-
     }
+
+    calculateNightlyTotal()
+    {
+        return this.numberOfNights() * this.element.dataset.nightlyPrice;
+    }
+
+    updateNightlyTotal()
+    {
+        this.numOfNightsTarget.textContent = this.numberOfNights();
+        this.nightlyTotalTarget.textContent = this.calculateNightlyTotal();
+        this.updateServiceFee();
+    }
+
+    calculateServiceFee()
+    {
+        return (this.calculateNightlyTotal() * this.element.dataset.serviceFeePercentage).toFixed(2);
+    }
+
+    updateServiceFee()
+    {
+        this.serviceFeeTarget.textContent = this.calculateServiceFee();
+        this.updateTotal();
+    }
+
+    updateTotal()
+    {
+        this.totalTarget.textContent = (+this.calculateNightlyTotal() + +this.element.dataset.cleaningFee + +this.calculateServiceFee()).toFixed(2);
+    }
+
+    numberOfNights()
+    {
+        if (isEmpty(this.checkinTarget.value) || isEmpty(this.checkoutTarget.value)) {
+            return 0;
+        }
+        const checkinDate = new Date(this.checkinTarget.value);
+        const checkoutDate = new Date(this.checkoutTarget.value);
+
+        return (checkoutDate - checkinDate) / (1000 * 60 * 60 * 24);
+    }
+
 }
