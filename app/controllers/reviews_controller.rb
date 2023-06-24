153f4d9071ebfb27c8f5cd056d0fdde1b6495d4f
Francisco Ferraz <ffranciscofferraz@gmail.com>
2023-06-24 20:03:08 +0100

    Reviews fixed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!
 
   def new
-    @property = Property.find(params[:property_id])
+    @property = Property.find_by(id: params[:property_id])
 
     if current_user.reviews.where(reviewable_id: @property.id).count == current_user.reservations.where(property_id: @property.id).count
       redirect_to root_path, alert: 'You have already reviewed this property.'
@@ -18,22 +18,12 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @property = Property.find(params[:review][:property_id])
 
-    if current_user.reviews.where(reviewable_id: @property.id).count == current_user.reservations.where(property_id: @property.id).count
-      redirect_to root_path, alert: 'You have already reviewed this property.'
-      return
-    elsif current_user.reservations.where(property_id: @property.id).empty?
-      redirect_to root_path, alert: 'You have not stayed at this property yet.'
-      return
-    end
-
-    @review = Review.new(review_params)
-    @review.user = current_user
-    @review.property = @property
+    @review = current_user.reviews.build(review_params)
+    @review.reviewable = Property.find_by(params[:property_id])
 
     if @review.save
-      redirect_to @property, notice: 'Review was successfully created.'
+      redirect_to property_path(@review.reviewable), notice: 'Review created successfully.'
     else
       render :new
     end
@@ -42,6 +32,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:rating, :title, :body)
+    params.require(:review).permit(:title, :body, :rating, :reviewable_id, :reviewable_type)
   end
 end
